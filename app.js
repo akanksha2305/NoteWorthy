b98eb56d6a98444c7a6aa763ceed38a333e5267f
akanksha2305 <itsakankshag@gmail.com>
2023-07-31 17:40:05 +0530

    authentication done using passport.js

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ require("dotenv").config();
 
 const express = require('express');
 var expressLayouts=require("express-ejs-layouts");
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app= express();
 const port = 8000 || process.env.PORT;
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  // Date.now() - 30 * 24 * 60 * 60 * 1000
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use( express.urlencoded({extended : true}));
 app.use(express.json());
@@ -15,7 +33,10 @@ app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
+connectDB(); 
+
 //Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
