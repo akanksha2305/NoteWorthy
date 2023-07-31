784972df1e50b2777942621688b7c1d29a10f6e7
akanksha2305 <itsakankshag@gmail.com>
2023-07-31 18:11:12 +0530

    complete project

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const express = require('express');
-var expressLayouts=require("express-ejs-layouts");
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require("method-override");
 const connectDB = require('./server/config/db');
 const session = require('express-session');
 const passport = require('passport');
 const MongoStore = require('connect-mongo');
 
-const app= express();
+const app = express();
 const port = 8000 || process.env.PORT;
 
 app.use(session({
@@ -24,26 +25,34 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use( express.urlencoded({extended : true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
+// Conntect to Database
+connectDB();  
 
+// Static Files
 app.use(express.static('public'));
 
+// Templating Engine
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-connectDB(); 
 
-//Routes
+
+// Routes
 app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
-app.get('*', function(req,res){
+// Handle 404
+app.get('*', function(req, res) {
+  //res.status(404).send('404 Page Not Found.')
   res.status(404).render('404');
 })
 
+
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-  });
+  console.log(`App listening on port ${port}`);
+});
